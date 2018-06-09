405803f6d6519c86a481274c7f39d6864542e8b0
conrad <r06942143@github.com>
2018-06-09 04:46:45 +0000

    fix

--- hw5/p3.py
+++ hw5/p3.py
@@ -7,68 +7,61 @@ from keras.callbacks import *
 from keras.applications.resnet50 import ResNet50
 from keras.regularizers import *
 import os
-
-train_path = '/root/data/DL_5/FullLengthVideos/videos/train/'
-
-folder = os.listdir(train_path)
+from skimage import io
 
 
 
+train_path = '/root/data/DL_5/HW5_data/FullLengthVideos/videos/train/'
+train_label_path = '/root/data/DL_5/HW5_data/FullLengthVideos/labels/train/'
+train_folder = sorted(os.listdir(train_path))
+
+valid_path = '/root/data/DL_5/HW5_data/FullLengthVideos/videos/valid/'
+valid_label_path = '/root/data/DL_5/HW5_data/FullLengthVideos/labels/valid/'
+valid_folder = sorted(os.listdir(valid_path))
 
 
-'''
+def get_data_label(path,label_path,folder_path):
+    for i,folder in enumerate(folder_path):
+        train_path = os.path.join(path,folder)
+        data = sorted(os.listdir(train_path))
+        frames = []
+        for i in range(len(data)):
+            frame = io.imread(os.path.join(train_path,data[i]))
+            frames.append(frame)
+        label = os.path.join(label_path,folder+'.txt')
+        
+    return np.array(frames).astype(np.uint8),np.genfromtxt(label,dtype = 'int')
 
+
+train_X,train_Y = get_data_label(train_path,train_label_path,train_folder)
+valid_X,valid_Y = get_data_label(valid_path,valid_label_path,valid_folder)
 
 
 
 input_tensor = Input(shape=(240,320,3))
-#model = InceptionV3(input_tensor = input_tensor,include_top=False, weights='imagenet')
 model = ResNet50(input_tensor=input_tensor,weights='imagenet', include_top=False)
 
 outcome = model.predict(train_X,verbose = 1)
 outcome_V = model.predict(valid_X,verbose =1)
-feature_X = np.zeros((len(bb),max(bb),2048))
-count = 0
-for i in range(len(bb)):
-    for j in range(bb[i]):
-        feature_X[i][j] = outcome[count]
-        count+=1
 
-
-feature_V = np.zeros((len(valid_bb),max(valid_bb),2048))
-count = 0
-for i in range(len(valid_bb)):
-    for j in range(valid_bb[i]):
-        feature_V[i][j] = outcome_V[count]
-        count+=1
 
 
 input_features = Input(batch_shape=(None,None,2048))
-x = Bidirectional(GRU(512,kernel_regularizer = l2(0.002),return_sequences = True))(input_features)
-x = Bidirectional(GRU(256,kernel_regularizer = l2(0.002),return_sequences = True))(x)
-x = Dense(11,kernel_regularizer = l2(0.002),activation = 'softmax')(x)
-x = Reshape((11,))(x)
-RNN_layer = Model(input_features,x)
-RNN_layer.summary()
+#x = Bidirectional(GRU(512,return_sequences=True))(iput_features)
+output,state_h = Bidirectional(GRU(256,return_state=True))(input_features)
+state_h = Dense(11,activation='softmax')(state_h)
+state_h = Reshape((11,))(state_h)
 
-#RNN_layer = Sequential()
-#RNN_layer.add(Input(shape=(None,2048)))
-#RNN_layer.add(Bidirectional(LSTM(256)))
-#RNN_layer.add(Bidirectional(LSTM(128)))
-#RNN_layer.add(Dense(11,activation = 'softmax'))
+videoRNN = Model(input_feature,state_h)
 
 
 
+videoRNN.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['acc'])
+
+checkpoint = ModelCheckpoint("./model/videos_RNN_model_{val_acc:.2f}.hdf5", monitor='val_acc', verbose=1, save_best_only=True, mode='max',period=1)
+
+videoRNN.fit(feature_X, train_Y, epochs=50, batch_size=64,validation_data=(feature_V,valid_Y),callbacks=[checkpoint],verbose=1)
+#see = videoRNN.predict(feature_V)
+#np.savetxt('./see.npy',np.round(see),delimiter=',')
 
 
-
-RNN_layer.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['acc'])
-
-checkpoint = ModelCheckpoint("./model/RNN_model_{val_acc:.2f}.hdf5", monitor='val_acc', verbose=1, save_best_only=True, mode='max',period=1)
-
-RNN_layer.fit(feature_X, train_Y, epochs=50, batch_size=64,validation_data=(feature_V,valid_Y),callbacks=[checkpoint],verbose=1)
-see = RNN_layer.predict(feature_V)
-np.savetxt('./see.npy',np.round(see),delimiter=',')
-
-
-'''
