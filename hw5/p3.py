fce917b333ff4ca81f34c68f57e77ea9712118f8
conrad <r06942143@github.com>
2018-06-10 14:46:51 +0000

    gogo

--- hw5/p3.py
+++ hw5/p3.py
@@ -16,12 +16,12 @@ def get_data_label(path,folder):
     print(folder)
     train_path = os.path.join(path,folder)
     data = sorted(os.listdir(train_path))
-    all_X = np.zeros((len(data),240,320,3),dtype=np.uint8)
     frames = []
     for j in range(len(data)):
         frame = io.imread(os.path.join(train_path,data[j]))
+        
         frames.append(frame)
-    return all_X
+    return np.array(frames).astype(np.uint8)
 
 
 
@@ -36,36 +36,37 @@ input_tensor = Input(shape=(240,320,3))
 model = ResNet50(input_tensor=input_tensor,weights='imagenet', include_top=False)
 videoRNN = load_model('./p3.hdf5')
 outcome_V1 = model.predict(valid_X1,verbose = 1)
-predict_X1 = videoRNN.predict(outcome_V1,verbose = 1)
-ans_1 = np.argmax(np.around(predict_X1),axis =1)
+predict_X1 = videoRNN.predict(outcome_V1.reshape((1,-1,2048)),verbose = 1)
+ans_1 = np.argmax((predict_X1),axis =2)
+print(ans_1)
 with open(os.path.join(sys.argv[2],str(valid_folder[0])+'.txt'), 'w') as f:
-    for i in ans_1:
+    for i in ans_1[0]:
         f.write('%d\n' %(i))
 
 outcome_V2 = model.predict(valid_X2,verbose = 1)
-predict_X2 = videoRNN.predict(outcome_V2,verbose = 1)
-ans_2 = np.argmax(np.around(predict_X2),axis =1)
+predict_X2 = videoRNN.predict(outcome_V2.reshape((1,-1,2048)),verbose = 1)
+ans_2 = np.argmax(np.around(predict_X2),axis =2)
 with open(os.path.join(sys.argv[2],str(valid_folder[1])+'.txt'), 'w') as f:
-    for i in ans_2:
+    for i in ans_2[0]:
         f.write('%d\n' %(i))
 
 outcome_V3 = model.predict(valid_X3,verbose = 1)
-predict_X3 = videoRNN.predict(outcome_V3,verbose = 1)
-ans_3 = np.argmax(np.around(predict_X3),axis =1)
+predict_X3 = videoRNN.predict(outcome_V3.reshape((1,-1,2048)),verbose = 1)
+ans_3 = np.argmax(np.around(predict_X3),axis =2)
 with open(os.path.join(sys.argv[2],str(valid_folder[2])+'.txt'), 'w') as f:
-    for i in ans_3:
+    for i in ans_3[0]:
         f.write('%d\n' %(i))
 
 outcome_V4 = model.predict(valid_X4,verbose = 1)
-predict_X4 = videoRNN.predict(outcome_V4,verbose = 1)
-ans_4 = np.argmax(np.around(predict_X4),axis =1)
+predict_X4 = videoRNN.predict(outcome_V4.reshape((1,-1,2048)),verbose = 1)
+ans_4 = np.argmax(np.around(predict_X4),axis =2)
 with open(os.path.join(sys.argv[2],str(valid_folder[3])+'.txt'), 'w') as f:
-    for i in ans_4:
+    for i in ans_4[0]:
         f.write('%d\n' %(i))
 outcome_V5 = model.predict(valid_X5,verbose = 1)
-predict_X5 = videoRNN.predict(outcome_V5,verbose = 1)
-ans_5 = np.argmax(np.around(predict_X5),axis =1)
+predict_X5 = videoRNN.predict(outcome_V5.reshape((1,-1,2048)),verbose = 1)
+ans_5 = np.argmax(np.around(predict_X5),axis =2)
 with open(os.path.join(sys.argv[2],str(valid_folder[4])+'.txt'), 'w') as f:
-    for i in ans_5:
+    for i in ans_5[0]:
         f.write('%d\n' %(i))
 
